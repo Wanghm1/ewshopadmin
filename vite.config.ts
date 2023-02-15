5b4f5f432d07dfcfd4a11a9decf5af677e048a13
whm <whm@whm>
2023-02-15 18:35:58 +0800

    'second'

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import { NaiveUiResolver } from "unplugin-vue-components/resolvers";
 
+import { resolve } from "path";
+
+// import * as process from "process";
+
+function pathResolve(dir: string) {
+  return resolve(process.cwd(), ".", dir);
+}
+
+console.log(pathResolve("src"));
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-})
+  plugins: [
+    vue(),
+    Components({
+      resolvers: [NaiveUiResolver()],
+    }),
+  ],
+  resolve: {
+    alias: [
+      {
+        find: "@",
+        replacement: pathResolve("src"),
+      },
+    ],
+  },
+  server: {
+    host: "localhost",
+    port: 8000,
+  },
+});
