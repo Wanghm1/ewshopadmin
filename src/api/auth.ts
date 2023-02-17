0da29fd07cb5fb4ab3f8b26af8161d0fd61e0676
whm <whm@whm>
2023-02-17 19:49:30 +0800

    sixth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -16,3 +16,11 @@ export function user() {
     method: "GET",
   });
 }
+
+//退出登录
+export function logout() {
+  return request({
+    url: "/api/auth/logout",
+    method: "POST",
+  });
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "@/views/Home.vue";
 import Login from "@/views/login/index.vue";
-import Dashboard from "@/views/dashboard/Dashboard.vue";
+
+// import Dashboard from "@/views/dashboard/Dashboard.vue";
+import dashboard from "@/router/modules/dashboard";
 
 // 2. 定义一些路由
 // 每个路由都需要映射到一个组件。
@@ -9,8 +11,10 @@ import Dashboard from "@/views/dashboard/Dashboard.vue";
 const routes = [
   { path: "/", component: Home },
   { path: "/login", name: "login", component: Login },
-  { path: "/dashboard", name: "dashboard", component: Dashboard },
+  // { path: "/dashboard", name: "dashboard", component: Dashboard },
 ];
+
+const baseRoutes = [...routes, ...dashboard];
 
 // 3. 创建路由实例并传递 `routes` 配置
 // 你可以在这里输入更多的配置，但我们在这里
@@ -18,7 +22,7 @@ const routes = [
 const router = createRouter({
   // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
   history: createWebHistory(),
-  routes, // `routes: routes` 的缩写
+  routes: baseRoutes, // `routes: routes` 的缩写
 });
 
 router.beforeEach((to, from, next) => {
